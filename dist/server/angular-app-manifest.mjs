d839f7cd0b75a560b333a50988450fce30811f14
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-09-30 11:12:27 +0300

    reset ui from national day

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -110,8 +110,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 16815, hash: '3af378d345b08772852f1b1874c3ed74660b35c8d63efd2c2fbd694d0252677e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5225, hash: 'e90e97c1aa9bb1ba90b513606f31808eb339304ac748cc1e9bef9f3923c85dab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-CTOAILB3.css': {size: 147728, hash: '1fH/RJ78urY', text: () => import('./assets-chunks/styles-CTOAILB3_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 16815, hash: 'a636b9e0b9c7c3ab1cad7bd4b4fe3aebc33b987fa36e714a9f8d7b6fa2776974', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5225, hash: '63f9a4c2ebe0da84f6d9c99e285c2d2c6ef6c0f8275f9b2f0d339a3d9f20aefc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-YOGF3AU4.css': {size: 147703, hash: 'yEIC6SSA34s', text: () => import('./assets-chunks/styles-YOGF3AU4_css.mjs').then(m => m.default)}
   },
 };
